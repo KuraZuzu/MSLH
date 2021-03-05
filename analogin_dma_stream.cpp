7e00ca660ed3aae8eeced716f899de2828a1ea4f
zuzukun <7bda2106@mail.u-tokai.ac.jp>
2021-03-05 10:10:49 +0900

    ADC配列を共有するためにstaticにしたが、配列の確保がうまくいってなさそう？最大値の65535が入る

--- analogin_dma_stream.cpp
+++ analogin_dma_stream.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "analogin_dma_stream.h"
+
+uint16_t* AnalogInDMAStream::_adc1_value;
+uint16_t* AnalogInDMAStream::_adc2_value;
+uint16_t* AnalogInDMAStream::_adc3_value;
+bool AnalogInDMAStream::_active_ADC1_flag = false;
+bool AnalogInDMAStream::_active_ADC2_flag = false;
+bool AnalogInDMAStream::_active_ADC3_flag = false;
--- analogin_dma_stream.h
+++ analogin_dma_stream.h
@@ -8,30 +8,52 @@
 #ifndef ZUZUHALFTPPMOD1_ANALOGIN_DMA_STREAM_H
 #define ZUZUHALFTPPMOD1_ANALOGIN_DMA_STREAM_H
 
-#include "basis_analogin_dma_stream.h"
+#include "adc.h"
 
 class AnalogInDMAStream {
 
-    AnalogInDMAStream(ADC_HandleTypeDef& hadc, uint32_t rank, BasisAnalogInDMAStream& analog_in)
-    : _hadc(hadc), _rank(rank), _shared_analog_in(analog_in)
+public:
+    AnalogInDMAStream(ADC_HandleTypeDef& hadc, uint32_t rank)
+    : _hadc(hadc), _rank(rank)
     {}
 
-    void init(){
-        //ここで Basis のADC+DMAstartを開始する。
-        _shared_analog_in.init(_hadc);
+    void start(){
+        if(_hadc.Instance == ADC1 && !_active_ADC1_flag) {
+            _adc1_value = new uint16_t[_hadc.Init.NbrOfConversion];
+            for (uint32_t i = 0; i < _hadc.Init.NbrOfConversion; ++i) _adc1_value[i] = 0;
+            HAL_ADC_Start_DMA(&_hadc, (uint32_t*)_adc1_value, _hadc.Init.NbrOfConversion);
+            _active_ADC1_flag = true;
+
+        }else if (_hadc.Instance == ADC2 && !_active_ADC2_flag) {
+            _adc2_value = new uint16_t[_hadc.Init.NbrOfConversion];
+            for (uint32_t i = 0; i < _hadc.Init.NbrOfConversion; ++i) _adc2_value[i] = 0;
+            HAL_ADC_Start_DMA(&_hadc, (uint32_t*)_adc2_value, _hadc.Init.NbrOfConversion);
+            _active_ADC2_flag = true;
+
+        }else if (_hadc.Instance == ADC3 && !_active_ADC3_flag) {
+            _adc3_value = new uint16_t[_hadc.Init.NbrOfConversion];
+            for (uint32_t i = 0; i < _hadc.Init.NbrOfConversion; ++i) _adc3_value[i] = 0;
+            HAL_ADC_Start_DMA(&_hadc, (uint32_t*)_adc3_value, _hadc.Init.NbrOfConversion);
+            _active_ADC3_flag = true;
+        }
     }
 
     uint16_t read() {
-        return _shared_analog_in.read(_hadc, _rank);
+        if(_hadc.Instance == ADC1) return _adc1_value[_rank];
+        else if(_hadc.Instance == ADC2) return _adc2_value[_rank];
+        else if(_hadc.Instance == ADC3) return _adc3_value[_rank];
+        else return 0;
     }
 
 private:
     ADC_HandleTypeDef& _hadc;
     uint32_t _rank;
-//    BasisAnalogInDMAStream& _temp_init_analog_in;
-    BasisAnalogInDMAStream& _shared_analog_in;
-    //なんか参照型にできない.
-
+    static uint16_t* _adc1_value;
+    static uint16_t* _adc2_value;
+    static uint16_t* _adc3_value;
+    static bool _active_ADC1_flag;
+    static bool _active_ADC2_flag;
+    static bool _active_ADC3_flag;
 
 };
 

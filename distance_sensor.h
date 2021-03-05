51d4d2a65f427bf8c011fcade7fb70e75cfe33c1
zuzukun <7bda2106@mail.u-tokai.ac.jp>
2021-03-05 17:22:10 +0900

    STM32CubeMX(.ioc)のピンラベルをつけた。名前つけだだけで機能は変わらない。

--- distance_sensor.h
+++ distance_sensor.h
@@ -7,24 +7,43 @@
 #define ZUZUHALFTPPMOD1_DISTANCE_SENSOR_H
 
 #include "analogin_dma_stream.h"
+#include "digitalout.h"
 
 /**
- * This is a distance sensor class using Morinaga-type light emitting circuit.
- * 森永式発光回路を用いた距離センサクラスです。
- *
+ * @brief
+ *   This is a distance sensor class using Morinaga-type light emitting circuit.  <br>
+ *   森永式発光回路を用いた距離センサクラスです．
  */
 class DistanceSensor {
+
 public:
+
     /**
-     * @param phtr is Photo-Transistor.
+     * @param phtr is Photo-Transistor adc handler.
      */
-//    DistanceSensor(AnalogInDMAStream phtr):_phtr(phtr) {
-//
-//    }
+    DistanceSensor(AnalogInDMAStream phtr, DigitalOut led)
+    :_phtr(phtr), _led(led) {
+        _phtr.start();
+    }
+
+    void flash() {
+
+    }
+
+    uint16_t get_distance_mm() {
+        return convert_12bit_to_mm(_phtr.read());
+    }
+
 
 private:
 
-//    AnalogInDMAStream _phtr;
+    uint16_t convert_12bit_to_mm(uint16_t value) {
+        value = 0; //ここで距離変換の数式わちゃわちゃ。
+        return value;
+    }
+
+    AnalogInDMAStream _phtr;
+    DigitalOut _led;
 
 };
 

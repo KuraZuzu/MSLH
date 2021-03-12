ac7cceba569ac245aa0671de5792ba3f23aea21b
zuzukun <7bda2106@mail.u-tokai.ac.jp>
2021-03-12 21:25:40 +0900

    モーターとエンコーダのドライブクラス試作

---
+++ motor_control.cpp
@@ -0,0 +1,10 @@
+/*!
+* @file motor_control.cpp v1.0
+* @Copyright © 2021 Kazushi Kurasawa
+* @date 2021.03.12
+*
+* Released under the MIT license.
+* see https://opensource.org/licenses/MIT
+*/
+
+#include "motor_control.h"

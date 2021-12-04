37c3f8651cb59b9149898ef8a85a0cf4f60a4a91
KuraZuzu <1ceim020@mail.u-tokai.ac.jp>
2021-12-04 12:58:19 +0900

    distance_sensor_2追加

---
+++ distance_sensor_2.cpp
@@ -0,0 +1,45 @@
+/*!
+ * @file distance_sensor.cpp v1.0
+ * @Copyright © 2021 Kazushi Kurasawa
+ * @date 2021.03.03
+ *
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+#include "distance_sensor.h"
+
+mslh::DistanceSensor::DistanceSensor(GPIO_TypeDef *led_x, uint16_t led_pin, AnalogInDMAStream photo_transistor)
+        : _photo_transistor(photo_transistor)
+        , _led_x(led_x)
+        , _led_pin(led_pin) {
+
+//    _photo_transistor.start();  //< まだstartしてないから読んだらバグる。
+//    HAL_GPIO_WritePin(_led_x, _led_pin, GPIO_PIN_SET);  //< ledは常に点灯させておくことでコンデンサに給電させない。
+}
+
+void mslh::DistanceSensor::start() {
+    _photo_transistor.start();
+}
+
+uint32_t mslh::DistanceSensor::read(uint32_t charge_time_ms) const {
+    //結局PWMがいいのではないか。 by Fさん。
+
+    HAL_GPIO_WritePin(_led_x, _led_pin, GPIO_PIN_RESET);;  //< コンデンサ充電開始
+    HAL_Delay(charge_time_ms); //< 1m秒が時定数 ( 47u[F] + 20[Ω] ) 63.2%充電 1.5m秒くらい欲しいかも？
+    const uint32_t offset_value = _photo_transistor.read();
+    HAL_GPIO_WritePin(_led_x, _led_pin, GPIO_PIN_SET);
+    HAL_Delay(1);  // < 波形がピークになるのをまつ まだ仮時間
+
+    return _photo_transistor.read() - offset_value;
+    // ここで一旦値を保存して getDistance_mm を呼ぶのがいいかも。
+}
+
+uint32_t mslh::DistanceSensor::getDistance_mm() {
+    return convert_12bit_to_mm(_photo_transistor.read());
+}
+
+uint32_t mslh::DistanceSensor::convert_12bit_to_mm(uint16_t value) {
+    value = 0; //ここで距離変換の数式わちゃわちゃ。
+    return value;
+}
---
+++ distance_sensor_2.h
@@ -0,0 +1,54 @@
+/*!
+ * @file distance_sensor.h v1.0
+ * @Copyright © 2021 Kazushi Kurasawa
+ * @date 2021.03.03
+ *
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+#ifndef MSLH_DISTANCE_SENSOR_H
+#define MSLH_DISTANCE_SENSOR_H
+
+#include "arm_math.h"
+#include "pwm_out.h"
+#include "analog_in_dma_stream.h"
+#include "digital_out.h"
+
+namespace mslh {
+
+/**
+ * @brief
+ *   This is a distance sensor class using Morinaga-type light emitting circuit.  <br>
+ *   森永式発光回路を用いた距離センサクラスです．
+ */
+class DistanceSensor {
+
+public:
+
+    /**
+     * @param photo_transistor is Photo-Transistor adc handler.
+     */
+    DistanceSensor(GPIO_TypeDef *led_x, uint16_t led_pin, AnalogInDMAStream photo_transistor);
+
+    void start();
+
+    /**
+     * @param Charge capacitor (can't set us unit).
+     */
+    uint32_t read(uint32_t charge_time_ms = 1) const;
+
+private:
+
+    uint32_t getDistance_mm();
+
+    uint32_t convert_12bit_to_mm(uint16_t value);
+
+    GPIO_TypeDef *_led_x;
+    const uint16_t _led_pin;
+    AnalogInDMAStream _photo_transistor;
+};
+
+}  // namespace mslh
+
+#endif //MSLH_DISTANCE_SENSOR_H

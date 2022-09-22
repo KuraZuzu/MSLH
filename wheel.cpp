787fe06198597a0e990984149ae85f91b70073bb
KuraZuzu <1ceim020@mail.u-tokai.ac.jp>
2022-09-23 05:39:51 +0900

    逆進できない理由が判明(ただし直せていない)

--- wheel.cpp
+++ wheel.cpp
@@ -54,55 +54,40 @@ void mslh::Wheel::setSpeed(float32_t accel, float32_t speed) {
 }
 
 void mslh::Wheel::interruptTwoFreedomDegreeControl() {
-
     /**
-     * @note 実加速度_accel計測
-     * @warning 今後の並進制御の方法によっては不要となる
-     */
-//    _accel = (_speed - _old_speed) / _speed_sampling_time; // 実加速度_accel計測
-//    _old_speed = _speed; // 実加速度_accel計測のための前回の割り込み時の速度コピー
-
-    /**
-     * @note 目標速度を超えていた場合、加速度を0にする
-     *   ①( (加速度 > 0) && ((目標速度 - 現在速度) > 0) ) の際に加速度有効
-     *   ②( (加速度 < 0) && ((目標速度 - 現在速度) < 0) ) の際に加速度有効
-     *   ①と②を満たさない時，加速度無効 (0) として速度のPID制御のみ行う(現在はP制御のみ)
-     */
+ * @note 目標速度を超えていた場合、加速度を0にする
+ *   ①(加速度 > 0) && ((目標速度 - 現在速度) > 0) の際に加速度計算 OK
+ *   ②(加速度 < 0) && ((目標速度 - 現在速度) < 0) の際に加速度計算 OK
+ *   ①と②を満たさない時，加速度を0として速度のPID制御のみ行う(現在はP制御のみ)
+ */
     if((_target_accel * (_target_speed - _speed)) <= 0.0f) _target_accel = 0.0f;
 
+    // PID制御のための差分算出
+    const float32_t diff_speed = _ideal_speed - _speed; // (目標速度) - (現在速度) motor-duty比調整のP制御のための差分．
+    const float32_t voltage_pid = diff_speed * machine_parameter::KP_MOTOR_VOLTAGE; // ゲインをかける
 
-    /**
-     * @note フィードバック制御
-     *   PID制御で目標回転数への補数を算出
-     *   フィードフォワードより前に来てるが、１回の割り込み分の遅れとなるため
-     *   フィードフォワード -> フィードバックの制御となっている
-     */
-    const float32_t diff_speed = _ideal_speed - _speed; // (現在の理想速度) - (現在の実速度) motor-duty比調整のP制御のための差分．
-    const float32_t pid_error = diff_speed * machine_parameter::KP_MOTOR_VOLTAGE; // ゲインをかける
-    _voltage += _voltage * pid_error; // ここで前回の速度計測からのフィードバック(P制御)をかける(フィードバック自体は前回のものを参考に補正をかける)
-
-
-    //指令通りの加速度による理想速度を計算(１つ前の割り込み時の速度フェイードバックなので、"フィードバック制御"より後に算出)
-    _ideal_speed += (_target_accel * _speed_sampling_time);
 
     /**
      * @note フィードフォワード制御
      *   加速度から必要な出力電圧を算出する(なぜかPIDのゲインによって特定の速度より上がらなくなる)
      *   必要wheelトルク = (質量) * (加速度) * (直径 [mm -> m]) / (2 [2つのモータのため]);
      */
-    const float32_t wheel_torque = machine_parameter::MASS * (_target_accel * 0.001f * _speed_sampling_time) * (machine_parameter::WHEEL_RADIUS * 0.001f) * 0.5f; // *0.5は2つのモータのため *0.001は[m]から[mm]への変換
+    const float32_t wheel_torque = machine_parameter::MASS * (_target_accel * 0.001f * _speed_sampling_time) * (machine_parameter::WHEEL_RADIUS * 0.001f) * 0.5f; // *0.5は2つのモータのため
     const float32_t motor_torque = wheel_torque * machine_parameter::GEAR_RATIO; //必要モータトルク
     const float32_t current = motor_torque / machine_parameter::K_T; // モータに必要な電流
-    const float32_t reverse_voltage = machine_parameter::K_E * (60.0f * _target_accel * 0.001f * _speed_sampling_time / (PI * machine_parameter::WHEEL_DIAMETER)); // 加速度を加算して１割り込み後の回転速度での算出
-    _voltage += machine_parameter::RESISTANCE_MOTOR * current + reverse_voltage; // (トルク要求電流値 * モータ抵抗) + モータ逆起電力
 
+    //ここが原因で動かなかった
+    _ideal_speed = _target_accel * _speed_sampling_time + _speed; //こちらの式だと逆進できる(ただし、前進でも不安定)
+//    _ideal_speed += (_target_accel * _speed_sampling_time); //理想速度に追従するバージョン(前進で安定するが、逆進できない)
 
-    // [バッテリ電圧を考慮したduty比算出]
-    //const float32_t battery_voltage = 4.0f; // バッテリ電圧を固定値としたもの
+    const float32_t reverse_voltage = machine_parameter::K_E * (60.0f * _target_accel * 0.001f * _speed_sampling_time / (PI * machine_parameter::WHEEL_DIAMETER)); // 現在の速度+目標加速度で想定される逆起電力算出
+    _voltage += (machine_parameter::RESISTANCE_MOTOR * current + reverse_voltage); // 必要電圧
+
+    _voltage += _voltage * voltage_pid; // ここで前回の速度計測からのフィードバック制御をかける(フィードバック自体は前回のものを参考に補正をかける)
+
+    // バッテリ電圧を考慮したduty比算出
     const float32_t battery_voltage = 3.3f * static_cast<float32_t>(_battery.read()) / 0x0FFF * machine_parameter::BATTERY_VOLTAGE_RATIO;
     const float32_t duty_ratio = _voltage / battery_voltage;
 
     _motor.update(duty_ratio); // モータに印加
-    // [debug]  printf()を呼ぶと、外部でDelay()を呼んだ際に、そのDelay()から抜けられない。
-    //printf("%6lf\r\n", _voltage); //重い処理なので数値確認にのみ使用可能
 }

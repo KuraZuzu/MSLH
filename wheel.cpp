6e4f7fea874292fb31e44dbe09be0b23224cff33
KuraZuzu <1ceim020@mail.u-tokai.ac.jp>
2022-09-25 05:26:50 +0900

    式の順番を整えた。これで前進はきれいに動作する。

--- wheel.cpp
+++ wheel.cpp
@@ -55,11 +55,11 @@ void mslh::Wheel::setSpeed(float32_t accel, float32_t speed) {
 
 void mslh::Wheel::interruptTwoFreedomDegreeControl() {
     /**
- * @note 目標速度を超えていた場合、加速度を0にする
- *   ①(加速度 > 0) && ((目標速度 - 現在速度) > 0) の際に加速度計算 OK
- *   ②(加速度 < 0) && ((目標速度 - 現在速度) < 0) の際に加速度計算 OK
- *   ①と②を満たさない時，加速度を0として速度のPID制御のみ行う(現在はP制御のみ)
- */
+     * @note 目標速度を超えていた場合、加速度を0にする
+     *   ①(加速度 > 0) && ((目標速度 - 現在速度) > 0) の際に加速度計算 OK
+     *   ②(加速度 < 0) && ((目標速度 - 現在速度) < 0) の際に加速度計算 OK
+     *   ①と②を満たさない時，加速度を0として速度のPID制御のみ行う(現在はP制御のみ)
+     */
     if((_target_accel * (_target_speed - _speed)) <= 0.0f) _target_accel = 0.0f;
 
     // PID制御のための差分算出
@@ -75,19 +75,18 @@ void mslh::Wheel::interruptTwoFreedomDegreeControl() {
     const float32_t wheel_torque = machine_parameter::MASS * (_target_accel * 0.001f * _speed_sampling_time) * (machine_parameter::WHEEL_RADIUS * 0.001f) * 0.5f; // *0.5は2つのモータのため
     const float32_t motor_torque = wheel_torque * machine_parameter::GEAR_RATIO; //必要モータトルク
     const float32_t current = motor_torque / machine_parameter::K_T; // モータに必要な電流
-
-    //ここが原因で動かなかった
-//    _ideal_speed = _target_accel * _speed_sampling_time + _speed; //こちらの式だと逆進できる(ただし、前進でも不安定)
-    _ideal_speed += (_target_accel * _speed_sampling_time); //理想速度に追従するバージョン(前進で安定するが、逆進できない)
-
     const float32_t reverse_voltage = machine_parameter::K_E * (60.0f * _target_accel * 0.001f * _speed_sampling_time / (PI * machine_parameter::WHEEL_DIAMETER)); // 現在の速度+目標加速度で想定される逆起電力算出
     _voltage += (machine_parameter::RESISTANCE_MOTOR * current + reverse_voltage); // 必要電圧
 
-//    _voltage += _voltage * voltage_pid; // 元のFBの位置
 
     // バッテリ電圧を考慮したduty比算出
     const float32_t battery_voltage = 3.3f * static_cast<float32_t>(_battery.read()) / 0x0FFF * machine_parameter::BATTERY_VOLTAGE_RATIO;
     const float32_t duty_ratio = _voltage / battery_voltage;
 
     _motor.update(duty_ratio); // モータに印加
+
+
+    //ここが原因で動かなかった
+//    _ideal_speed = _target_accel * _speed_sampling_time + _speed; //こちらの式だと逆進できる(ただし、前進でも不安定)
+    _ideal_speed += (_target_accel * _speed_sampling_time); //理想速度に追従するバージョン(前進で安定するが、逆進できない)
 }

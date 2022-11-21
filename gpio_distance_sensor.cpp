0b1013071f90ef2d8b69aae9c9fd350899a72262
KuraZuzu <1ceim020@mail.u-tokai.ac.jp>
2022-11-22 01:49:59 +0900

    距離センサがおかしいのはソフトかハードか原因不明

--- gpio_distance_sensor.cpp
+++ gpio_distance_sensor.cpp
@@ -29,21 +29,32 @@ uint16_t mslh::GPIODistanceSensor::read(const uint32_t charge_time_us) {
     uint16_t peak_value = 0;
     uint16_t temp_value = 0;
 
-    /** コンデンサにチャージ */
-    while((_timer.getTime() - _charge_start_time) < charge_time_us) {} //< コンデンサの充電がフルになるまで待機
-    const uint16_t offset_value = _photo_transistor.read();  //< オフセット値取得
+    /** コンデンサのチャージ時間が指定より超過していた場合に放電する */
+    if(charge_time_us < (_timer.getTime() - _charge_start_time)) {
+        _led.write(1);
+        _timer.waitMicroSeconds(10);  //< この時間はマジックナンバーだが、恐らくどれでもOK(D-S間の抵抗値によるRC回路の時定数)
+        _led.write(0);
+        _charge_start_time = _timer.getTime();
+    }
+
+    /** 指定時間に他入りていない分をコンデンサにチャージ */
+    while((_timer.getTime() - _charge_start_time) < charge_time_us) { //< 指定時間までコンデンサの充電待機
+    }
+    const uint16_t offset_value = _photo_transistor.read();  //< センサのオフセット値取得
 
     /** LEDを点灯して計測開始 */
     _led.write(1);
     const uint32_t sampling_start_time = _timer.getTime();
-    while((_timer.getTime() - sampling_start_time) < 12) {  //< 波形がピークになるのを待つ(12はセンサによる立ち上がり時間マジックナンバー)
-        temp_value = _photo_transistor.read();  //< ピーク値取得
+    while((_timer.getTime() - sampling_start_time) <20) {  //< 波形がピークになるのを待つ(12はセンサによる立ち上がり時間マジックナンバー)
+        temp_value = _photo_transistor.read();  //< センサのピーク値取得
         if(peak_value < temp_value) peak_value = temp_value;
     }
+
+    /** LEDを消灯してコンデンサへの充電開始 */
     _led.write(0);
     _charge_start_time = _timer.getTime();
 
-//    return peak_value - offset_value;
-    return peak_value;
-//    return offset_value;
+    return peak_value - offset_value;
+//    return peak_value;    //< デバッグ用
+//    return offset_value;  //<デバッグ用
 }

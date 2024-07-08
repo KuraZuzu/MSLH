e7b6eeb9a2e97c40e658e8ffed473f39d7c60683
kurasawa <kurasawa@rt-net.jp>
2024-07-08 11:18:03 +0900

    ジャイロの零点ドリフトのキャリブレーションを追加

--- imu_icm42688p.h
+++ imu_icm42688p.h
@@ -14,7 +14,6 @@
 #include "spi.h"
 #include "digital_out.h"
 
-
 namespace mslh {
 
 class IMUICM42688P {
@@ -24,7 +23,9 @@ public:
     : _hspi(hspi)
     , _cs_x(cs_x)
     , _cs_pin(cs_pin)
-    , _accel_range(2.0f) {
+    , _accel_range(2.0f)
+    , _gyro_range(1000.0f)
+    , _gyro_yaw_offset(0.0f) {
     }
 
     void init() {
@@ -33,19 +34,33 @@ public:
         setConfigs();
     }
 
+    /* ジャイロの零点ドリフト補正 */
+    void calibrate() {
+        float32_t tmp_value = 0;
+        for (int i = 0; i < 100; i++) tmp_value += getAngularYaw();
+        _gyro_yaw_offset = tmp_value / 100.0f;
+    }
+
     uint8_t getWhoAmI() {
         const uint8_t address = 0x75;   //< Input who_am_i: 0x75, return: 0x47
         return read(address);  // 受け取るのは2バイト目のデータだけ
     }
 
+    float32_t getAngularYaw() {
+        // 0x29: upper Byte,  0x2A: lower Byte
+        const int16_t raw_angular_z = static_cast<int16_t>(((read(0x29) << 8) | read(0x2A)));
+        const float32_t _gyro_yaw = (_gyro_range* static_cast<float32_t>(raw_angular_z) / static_cast<float32_t>(INT16_MAX));
+        return _gyro_yaw - _gyro_yaw_offset;
+    }
+
     float32_t getAccelX() {
-        // 0x23: upper Byte,  0x24: lower Byte
+        // 0x1F: upper Byte,  0x20: lower Byte
         const int16_t raw_accel_x = static_cast<int16_t>(((read(0x1F) << 8) | read(0x20)));
         return (_accel_range* static_cast<float32_t>(raw_accel_x) / static_cast<float32_t>(INT16_MAX));
     }
 
     float32_t getAccelY() {
-        // 0x23: upper Byte,  0x24: lower Byte
+        // 0x21: upper Byte,  0x22: lower Byte
         const int16_t raw_accel_y =  static_cast<int16_t>((read(0x21) << 8) | read(0x22));
         return (_accel_range * static_cast<float32_t>(raw_accel_y) / static_cast<float32_t>(INT16_MAX));
     }
@@ -102,9 +117,18 @@ private:
         HAL_Delay(10);
         setPowerManage();
         HAL_Delay(10);
+        setGyroConfig();
+        HAL_Delay(10);
         setAccelConfig();
         HAL_Delay(10);
     }
+
+    void setGyroConfig() {
+        constexpr uint8_t gyro_conf_addr = 0x4F;
+        constexpr uint8_t gyro_conf_data = 0b00100110;  // range:1000[deg/s], rate: 1k[Hz]
+        write(gyro_conf_addr, gyro_conf_data);
+    }
+
 
     // 加速度のレンジの設定
     void setAccelConfig() {
@@ -125,7 +149,8 @@ private:
     GPIO_TypeDef *_cs_x;
     const uint16_t _cs_pin;
     float32_t _accel_range;  // default: 2.0f
-    float32_t _imu_range;
+    float32_t _gyro_range;
+    float32_t _gyro_yaw_offset;
 };
 
 }  // namespace mslh

1829b6c39bebea76bad797d37de7540b1e0b70c5
KuraZuzu <kurazuzukun3@gmail.com>
2024-12-06 11:30:25 +0900

    フレームごとにWheelの速度を指定するように雛形の変更

--- wheel_controller.cpp
+++ wheel_controller.cpp
@@ -9,114 +9,14 @@
 
 #include "wheel_controller.h"
 
-mslh::WheelController::WheelController(Motor &motor, Encoder &encoder, AnalogInDMAStream &battery, float32_t wheel_diameter, float32_t sampling_time)
-    : _motor(motor)
-    , _encoder(encoder)
-    , _battery(battery)
-    , _target_accel(0.0f)
-    , _linear_velocity(0.0f)
-    , _init_velocity(0.0f)
-    , _ideal_velocity(0.0f)
-    , _target_velocity(0.0f)
-    , _integral_diff_velocity(0.0f)
-    , _duty_ratio(0.0f)
-    , _sampling_time(sampling_time)
-    , _distance_per_pulse(wheel_diameter * PI / static_cast<float32_t>(_encoder.getOneRotationPulse()))
-    , _velocity_per_pulse(_distance_per_pulse / _sampling_time)
-    , _correction_velocity(0.0f)
-    {}
-
-void mslh::WheelController::start()
-{
-    _battery.init();
-    _encoder.start();
-    _motor.start();
-    _motor.update(0.0f);
-    _encoder.reset();
-}
-
-void mslh::WheelController::stop()
-{
-    _motor.stop();
-    _encoder.stop();
-}
-
-void mslh::WheelController::reset()
-{
-    _motor.update(0.0f);
-    _linear_velocity = 0.0f;
-    _target_accel = 0.0f;
-    _ideal_velocity = 0.0f;
-    _target_velocity = 0.0f;
-    _integral_diff_velocity = 0.0f;
-    _init_velocity = 0.0f;
-    _duty_ratio = 0.0f;
-    _encoder.reset();
-}
-
-void mslh::WheelController::orderVelocityMomentarily()
-{
-    setVelocity(0.0f, 0.0f);
-    _motor.update(0.0f);
-}
-
-void mslh::WheelController::setVelocity(float32_t accel, float32_t velocity)
-{
-    _target_accel = accel;
-    _init_velocity = _linear_velocity;
-    _target_velocity = velocity;
-}
-
-void mslh::WheelController::interrupt2DoFControll()
-{
-
-    /**
-     * @note フィードバック制御
-     *   FF制御より先に呼び出すが、初回は理想加速度が考慮されていないのでFBは２回目以降から実行される。
-     *   PID制御を実行
-     */
-    const float32_t diff_velocity = _ideal_velocity - _linear_velocity;
-    _integral_diff_velocity += diff_velocity;                                                // (目標速度) - (現在速度)　逆進の際は負の値となる
-    const float32_t p_error = diff_velocity * machine_parameter::KP_MOTOR_VOLTAGE;           // ゲインをかける (逆進の際は負の値となる)
-    const float32_t i_error = _integral_diff_velocity * machine_parameter::KI_MOTOR_VOLTAGE; // ゲインをかける (逆進の際は負の値となる)
-
-    // 現在速度が目標を超えていた場合に指令加速度を"0"にして当フレーム以降での理想速度に加算されないようにしてPIDのみの補正を適用
-    // （差分を積とすることで正負の両方に対応）
-    if(((_target_velocity-_init_velocity)*(_target_velocity-_linear_velocity)) <= 0) {
-        _target_accel = 0;
-        _ideal_velocity  = _target_velocity;
-    }
-
-    // 指定された加速度での理想速度算出
-    _ideal_velocity += (_target_accel * _sampling_time);
-
-    // PIDをモータ印加電圧に反映
-    const float32_t corrected_velocity = _ideal_velocity + p_error + i_error + _correction_velocity;
-    _correction_velocity = 0.0f;
-    // ここのcorrected_veocityに直接アクセスできる関数を実装して、publicにする。一時的に
-
-
-    /**
-     * @note フィードフォワード制御
-     *   加速度から必要な出力電圧を算出する(なぜかPIDのゲインによって特定の速度より上がらなくなる)
-     *   ① 加速Wheelトルク = (機体質量) * (タイヤ加速度[mm/ss -> m/ss]) * (タイヤ直径 [mm -> m]) / (2 [2つのモータのため]);
-     *   ② 定速度Wheel電圧 = Ke(逆起電力定数[V/rpm]) * (PIDを考慮したモータ回転数 [mm/s -> rpm]  ギア比も考慮)
-     */
-    // ① 加速Wheelトルク
-    const float32_t wheel_torque = machine_parameter::MASS * _target_accel * 0.001f * (machine_parameter::WHEEL_RADIUS * 0.001f) * 0.5f; //  *0.001f は[mm]->[m]変換  *0.5f は2つのモータのため
-    const float32_t motor_torque = wheel_torque / machine_parameter::GEAR_RATIO;                                                         // 必要モータトルク
-    float32_t current = 0.0f;
-    if(machine_parameter::FEED_FORWARD_FLAG) current = motor_torque / machine_parameter::K_T;  // モータに必要な電流
-
-    // ② 定速度Wheel電圧
-    const float32_t reverse_voltage = (machine_parameter::K_E * (60.0f * corrected_velocity) * machine_parameter::GEAR_RATIO) / (PI * machine_parameter::WHEEL_DIAMETER); // 分子の速度の単位を[mm]のままなのは、分母のタイヤ径も[mm]のため
-    const float32_t voltage = (machine_parameter::RESISTANCE_MOTOR * current + reverse_voltage);  // 必要電圧
-
-    /** バッテリ電圧を考慮したduty比算出 */
-    const float32_t battery_voltage = 3.3f * static_cast<float32_t>(_battery.read()) / 0x0FFF * machine_parameter::BATTERY_VOLTAGE_RATIO;
-    if(battery_voltage > 0.0f) _duty_ratio = voltage / battery_voltage;
-    else _duty_ratio = 0.0f;
-
-    /** モータに電圧印加 */
-    _motor.update(_duty_ratio);
-}
+// mslh::WheelController::WheelController(Motor &motor, Encoder &encoder, AnalogInDMAStream &battery, float32_t wheel_diameter, float32_t sampling_time)
+//     : _motor(motor)
+//     , _encoder(encoder)
+//     , _battery(battery)
+//     , _target_accel(0.0f)
+//     , _duty_ratio(0.0f)
+//     , _sampling_time(sampling_time)
+//     , _distance_per_pulse(wheel_diameter * PI / static_cast<float32_t>(_encoder.getOneRotationPulse()))
+//     , _velocity_per_pulse(_distance_per_pulse / _sampling_time)
+//     , _correction_velocity(0.0f)
+//     {}
--- wheel_controller.h
+++ wheel_controller.h
@@ -1,181 +1,95 @@
 /*!
-* @file motor_control.h v1.0
-* @Copyright © 2021 Kazushi Kurasawa
-* @date 2021.03.12
-*
-* Released under the MIT license.
-* see https://opensource.org/licenses/MIT
-*/
+ * @file motor_control.h v1.0
+ * @Copyright © 2021 Kazushi Kurasawa
+ * @date 2021.03.12
+ *
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
 
 #ifndef MSLH_WHEEL_CONTROLER_H
 #define MSLH_WHEEL_CONTROLER_H
 
-#include "arm_math.h"
-#include "defines.h"
-#include "motor.h"
-#include "encoder.h"
-#include "analog_in_dma_stream.h"
-#include "parameter.h"
 #include <cmath>
 
+#include "analog_in_dma_stream.h"
+#include "arm_math.h"
+#include "defines.h"
+#include "encoder.h"
+#include "motor.h"
+#include "parameter.h"
+
 namespace mslh {
-
-/**
- * @warning
- *     For all instances of the generated Wheel class　
- *     interruptMeasureSpeed() must be called at explicit intervals such as timer interrupt.
- *
- * Example:
- * @code
- *
- *   using namespace mslh;
- *
- * 　 // This definition is out of main().
- *   Interrupter<Wheel> wheel_interrupt;
- *
- *
- *   // Need ticker for measure speed.
- *   // This interrupt intervals are set in .ioc-file (timer.c).
- *   void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
- *
- *       // [ TIM6 (&htim6) ]
- *       // This callback function is Called every 10[ms].
- *       // 90M[Hz](APB1) / 36000 = 2500[Hz]
- *       // 2500[Hz] / 25(Period) = 100[Hz]   -> 10[ms]
- *       if(htim == &htim6) {
- *           wheel_interrupt.runSpecifiedDistance();
- *       }
- *
- *
- *   int main() {
- *       // Abbreviation Microcomputer startup settings.
- *       MX_TIM1_Init();                 //< Need setup HAL motor timer parameters.
- *       MX_TIM3_Init();                 //< Need setup HAL encoder timer parameters.
- *       MX_GPIO_Init();                 //< Need setup HAL_GPIO for motor _direction.
- *       MX_TIM6_Init();                 //< Need setup HAL ticker(callback) timer parameters.
- *
- *       Motor motor(htim1, TIM_CHANNEL_1, GPIOA,  GPIO_PIN_6, true);
- *       Encoder encoder(htim4, 500, false);
- *       AnalogInDMAStream battery(hadc1, 5);
- *       Wheel wheel(motor, encoder, battery, 13.5f, 0.01f);
- *
- *       wheel_interrupt.attach(&wheel, &Wheel::interruptControlWheel);  //< Attach function of interrupt.
- *
- *
- *       wheel.start();                                  //< Need start motor and encoder.
- *       wheel.setSpeed(500.0f, 100.0f);                 //< Running (wheel rotation) at a specified accel[mm/ss] and speed[mm/s].
- *       float32_t wheel_speed = test_wheel.getSpeed();  //< measure wheel speed
- *       wheel.stop()                                    //< Stop all work (Encoder values are retained).
- *   }
- * @endcode
- */
 class WheelController {
+   public:
+    WheelController(Motor &motor, Encoder &encoder, AnalogInDMAStream &battery,
+                    float32_t wheel_diameter, float32_t sampling_time)
+        : _motor(motor),
+          _encoder(encoder),
+          _battery(battery),
+          _target_accel(0.0f),
+          _duty_ratio(0.0f),
+          _sampling_time(sampling_time),
+          _distance_per_pulse(
+              wheel_diameter * PI /
+              static_cast<float32_t>(_encoder.getOneRotationPulse())),
+          _velocity_per_pulse(_distance_per_pulse / _sampling_time) {}
 
-public:
-    /**
-    * @note Wheel(___ , ___ , float32_t wheel_diameter, uint16_t speed_sampling_time);
-    *
-    * @param
-    *   wheel_diameter     : milli meter [mm].
-    *   speed_sampling_time: second [s].
-    */
-    WheelController(Motor &motor, Encoder &encoder, AnalogInDMAStream &battery, float32_t wheel_diameter, float32_t sampling_time);
-
-
-    /**
-     * @fn モータの速度計測&制御を行う，speed_sampling_time の間隔で実行
-     * @warning この関数をタイマ割り込み(任意の周期)で計測する
-     */
-    inline void interruptControlWheel() {
-        interruptMeasureVelocity();
-        interrupt2DoFControll();
+    void init() {
+        _battery.init();
+        _encoder.reset();
     }
 
-    /**
-     * @fn モータとエンコーダの動作開始．
-     */
-    void start();
-
-    /**
-     * @fn モータとエンコーダの動作停止．
-     */
-    void stop();
-
-    /**
-     * @fn モータとエンコーダの動作停止．
-     */
-    void orderVelocityMomentarily();
-
-    /**
-     * @fn モータとエンコーダ，_speedのリセット．
-     */
-    void reset();
-
-    /**
-     * @fn 指定した加速度と速度でモータ回転．
-     * @arg (±目標加速度, ±目標速度)
-     * @warning 呼び出しは1回で良い．
-     */
-    void setVelocity(float32_t accel, float32_t velocity);
-
-    [[gnu::warn_unused_result]] inline float32_t getVelocity() const { return _linear_velocity; }
-
-    // [[gnu::warn_unused_result]] inline float32_t getTargetAccel() const { return _target_accel; }
-
-    // [[gnu::warn_unused_result]] inline float32_t getTargetVelocity() const { return _target_velocity; }
-
-    void debugConsole() {
-        printf("[ENCODER]%5d,  MOTOR is FW now!", _encoder.getTotalPulse());
+    void start() {
+        _encoder.start();
+        _motor.start();
+        _motor.update(0.0f);
     }
 
-    void debugParams() {
-        printf("target_accel: %f\r\n", _target_accel);
-        printf("init_v: %f\r\n", _init_velocity);
-        printf("target_v: %f\r\n", _target_velocity);
+    void stop() {
+        _motor.stop();
+        _encoder.stop();
     }
 
-    void addCorrectionVelocity(float32_t correction_velocity) {
-        _correction_velocity += correction_velocity;
+    void reset() { _encoder.reset(); }
+
+    void setVelocity(float32_t velocity) { _target_velocity = velocity; }
+
+    float32_t getVelocity() { return _velocity; }
+
+    void controlWheelISR(float32_t velocity) {
+        // ここで速度更新したい
+        controlFeedBackISR();
+        controlFeedForwardISR();
+        _motor.update(_duty_ratio);
+        controlMeasureVelocityISR();
     }
 
-private:
-    /**
-     * @fn 速度計測をする．speed_sampling_time の間隔で実行．
-     * @warning この関数をタイマ割り込み(任意の周期)で計測する．
-     */
-    inline void interruptMeasureVelocity() {
+   private:
+       void controlMeasureVelocityISR() {
         _encoder.update();
-        _linear_velocity = _velocity_per_pulse * static_cast<float32_t>(_encoder.getDeltaPulse());
+        _velocity == _velocity_per_pulse *static_cast<float32_t>(
+                         _encoder.getDeltaPulse());
+        _target_accel = _velocity - _velocity;
     }
 
+    void controlFeedForwardISR();
 
-    /**
-     * @fn モータの速度制御をする．speed_sampling_time の間隔で実行．
-     * @note 2自由度制御(フィードフォワード & フィードバック)で構成される．
-     * @warning この関数をタイマ割り込み(任意の周期)で計測する．
-     *
-     * なぜか、5000[mm/s]あたりで速度が頭打ちとなる．PIDのゲインが釣り合っていまうタイミングがあるのかも．
-     */
-    void interrupt2DoFControll();
+    void controlFeedBackISR();
 
-
-    float32_t _target_accel;      //< 目標加速度(指令値)
-    float32_t _linear_velocity;          //< 現在の計測した実速度 [mm/s] (mm per second).
-    float32_t _ideal_velocity;    //< 理想速度(逐次、現在の測定速度に指令加速度を加算している)
-    float32_t _target_velocity;   //< 目標速度(指令値)
-    float32_t _integral_diff_velocity; //< I制御のための速度偏差積分
-    float32_t _init_velocity; //< setVelocity()をした時点での速度
+    float32_t _target_accel;
+    float32_t _target_velocity;
+    float32_t _velocity;
     float32_t _duty_ratio;
     Encoder &_encoder;
     Motor &_motor;
     AnalogInDMAStream &_battery;
-    const float32_t _sampling_time; //< second [s]
-    const float32_t _distance_per_pulse;  //< [mm/pulse] 1パルスにつき進む距離[mm]、距離計測の最低単位
-    const float32_t _velocity_per_pulse;     //< callbackされるサンプリングタイムも考慮した値、速度計測の最低単位
-    float32_t _correction_velocity;
+    const float32_t _sampling_time;       //< second [s]
+    const float32_t _distance_per_pulse;  //< [mm/pulse]
+    const float32_t _velocity_per_pulse;  //< [mm/s]
 
 };  // class WheelController
 
 }  // namespace mslh
 
-#endif //MSLH_WHEEL_CONTROLLER_H
+#endif  // MSLH_WHEEL_CONTROLLER_H

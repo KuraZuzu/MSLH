e3410521059ba67f12b4a0291292f2eb2baac42c
KuraZuzu <kurazuzukun3@gmail.com>
2024-12-07 21:54:31 +0900

    BatteryのAnaloginの変数名変更

--- battery.h
+++ battery.h
@@ -7,19 +7,19 @@
 namespace mslh {
 class Battery {
    public:
-    Battery(AnalogInDMAStream& battery, float32_t adc_ratio)
-        : _battery(battery), _adc_ratio(adc_ratio) {}
+    Battery(AnalogInDMAStream& analogin_dma_stream, float32_t adc_ratio)
+        : _analogin_dma_stream(analogin_dma_stream), _adc_ratio(adc_ratio) {}
 
     void init() { 
-        _battery.init(); 
+        _analogin_dma_stream.init(); 
     }
 
     inline float32_t readVoltage() {
-        return 3.3f * static_cast<float32_t>(_battery.read()) / 0x0FFF / _adc_ratio;
+        return 3.3f * static_cast<float32_t>(_analogin_dma_stream.read()) / 0x0FFF / _adc_ratio;
     }
 
    private:
-    AnalogInDMAStream _battery;
+    AnalogInDMAStream _analogin_dma_stream;
     float32_t _adc_ratio;  // 0 ~ 1.0（バッテリ電圧を監視するADC分圧比率）
 };
 
